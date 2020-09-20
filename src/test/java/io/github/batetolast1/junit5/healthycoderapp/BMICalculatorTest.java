ef667eaec6bd039473f9f3a1ca860a0ed11a02ce
batetolast1 <batetolast1@gmail.com>
2020-09-20 17:19:50 +0200

    JUnit 5 advanced concepts - some useful annotations

--- src/test/java/io/github/batetolast1/junit5/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/io/github/batetolast1/junit5/healthycoderapp/BMICalculatorTest.java
@@ -2,10 +2,9 @@
 
 package io.github.batetolast1.junit5.healthycoderapp;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -113,9 +112,12 @@ class BMICalculatorTest { // class name + "Test"
     }
 
     @Nested
+    @DisplayName("{{}} sample inner class display name") // @DisplayName allows to add custom name to test
     class FindCoderWithWorstBMITests {
         // assertAll()
         @Test
+        @DisplayName(">>>>> sample method display name")
+        @Disabled // @Disabled allows to disable test
         void should_ReturnCoderWithWorstBMI_When_CoderListNotEmpty() {
             // given
             // Assumptions - allows to execute test only when certain conditions are met (ie. when project is moved from development to production
@@ -144,6 +146,7 @@ class BMICalculatorTest { // class name + "Test"
 
         // performance unit test
         @Test
+        @DisabledOnOs(OS.LINUX) // allows to disable test in certain operating systems
         void should_ReturnCoderWithWorstBMIIn1Ms_When_CoderListHas10000Elements() {
             // given
             CoderDAO coderDAO = new CoderDAO();
